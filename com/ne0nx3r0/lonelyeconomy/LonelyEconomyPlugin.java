055a8aa65f47fa24442429eb74feb5c2d0b72918
Ne0nx3r0 <Ne0nx3r0@viasmooth.om.cox.net>
2014-04-11 19:47:28 -0500

    Got create table working, removing MySQL library

--- com/ne0nx3r0/lonelyeconomy/LonelyEconomyPlugin.java
+++ com/ne0nx3r0/lonelyeconomy/LonelyEconomyPlugin.java
@@ -2,6 +2,11 @@ package com.ne0nx3r0.lonelyeconomy;
 
 import com.ne0nx3r0.lonelyeconomy.commands.LonelyCommandExecutor;
 import com.ne0nx3r0.lonelyeconomy.economy.LonelyEconomy;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,6 +16,22 @@ public class LonelyEconomyPlugin extends JavaPlugin {
     
     @Override
     public void onEnable() {
+        try {
+            getDataFolder().mkdirs();
+
+            File configFile = new File(getDataFolder(),"config.yml");
+
+            if(!configFile.exists())
+            {
+                copy(getResource("config.yml"), configFile);
+            }
+        } 
+        catch (IOException ex) {
+            this.getLogger().log(Level.INFO, "Unable to load config!");
+            
+            return;
+        }
+        
         try {
             this.economy = new LonelyEconomy(this);
         } 
@@ -36,5 +57,18 @@ public class LonelyEconomyPlugin extends JavaPlugin {
     
     public LonelyEconomy getEconomy() {
         return this.economy;
+    }
+
+    public void copy(InputStream in, File file) throws IOException
+    {
+        OutputStream out = new FileOutputStream(file);
+        byte[] buf = new byte[1024];
+        int len;
+        while((len=in.read(buf))>0)
+        {
+            out.write(buf,0,len);
+        }
+        out.close();
+        in.close();
     }
 }
--- com/ne0nx3r0/lonelyeconomy/economy/LonelyEconomy.java
+++ com/ne0nx3r0/lonelyeconomy/economy/LonelyEconomy.java
@@ -1,75 +1,100 @@
 package com.ne0nx3r0.lonelyeconomy.economy;
 
+import java.sql.Connection;
 import com.ne0nx3r0.lonelyeconomy.LonelyEconomyPlugin;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
 import java.math.BigDecimal;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import lib.PatPeter.SQLibrary.MySQL;
-import net.minecraft.util.org.apache.commons.io.IOUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class LonelyEconomy {
-    private final MySQL connection;
+    private Connection con;
     private final Logger logger;
     
+    private final String TBL_ACCOUNTS;
+    private final String TBL_TRANSACTIONS;
+    private final String TBL_SERVER_BALANCE;
+
     public LonelyEconomy(LonelyEconomyPlugin plugin) throws SQLException {  
         this.logger = plugin.getLogger();
         
         ConfigurationSection dbConfig = plugin.getConfig().getConfigurationSection("database");
         
-        String prefix = dbConfig.getString("prefix");
-        String hostname = dbConfig.getString("hostname");
-        String port = dbConfig.getString("port");
+        String prefix = dbConfig.getString("prefix","");
+        String hostname = dbConfig.getString("hostname","localhost");
+        String port = dbConfig.getString("port","3306");
         String database = dbConfig.getString("database");
         String username = dbConfig.getString("username");
         String password = dbConfig.getString("password");
+        
+        this.TBL_ACCOUNTS = prefix+"accounts";
+        this.TBL_TRANSACTIONS = prefix+"transactions";
+        this.TBL_SERVER_BALANCE = prefix+"server_balance";
                 
-        this.connection = new MySQL(plugin.getLogger(),prefix,hostname,port,database,username,password);
-        
-        this.connection.open();
-
-        if(!this.createTablesIfNeeded()) {
-            // exception message should be right above this
-            this.logger.log(Level.WARNING, "Unable to see if the database tables need to be created and create them if needed!");
-        }
-    }
-    
-    private boolean createTablesIfNeeded(){
-        InputStream inputStream = getClass().getResourceAsStream("/createTables.sql");
-        
-        StringWriter writer = new StringWriter();
-        
-        try {
-            IOUtils.copy(inputStream, writer);
-        } 
-        catch (IOException ex) {
+	try {
+            Class.forName("com.mysql.jdbc.Driver");
+	} 
+        catch (ClassNotFoundException ex) {
             this.logger.log(Level.SEVERE, null, ex);
             
-            return false;
-        }
+            this.logger.log(Level.SEVERE,"No MySQL JDBC driver found (that's bad)");
+            
+            return;
+	}
         
-        String queries = writer.toString();
-        
-        PreparedStatement statement;
-        
-        try {
-            statement = this.connection.prepare(queries);
-            statement.execute();
-        } 
+	try {
+		this.con = DriverManager.getConnection("jdbc:mysql://"+hostname+":"+port+"/"+database,username,password);
+	} 
         catch (SQLException ex) {
             this.logger.log(Level.SEVERE, null, ex);
+
+            System.out.println("Database connection failed!");
+
+            return;
+	}
+ 
+	if (this.con == null) {
+            this.logger.log(Level.SEVERE,"Unable to connect to the database");
             
-            return false;
-        }
+            return;
+	}
+
+        ResultSet tableExistsResultSet = this.con.getMetaData().getTables(null, null, this.TBL_SERVER_BALANCE, null);
         
-        return true;
+        if(!tableExistsResultSet.next()) {
+            this.con.setAutoCommit(false);
+            
+            PreparedStatement createAccountsTable = this.con.prepareStatement("CREATE TABLE IF NOT EXISTS `"+this.TBL_ACCOUNTS+"` (  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,  `username` VARCHAR(45) NOT NULL,  `uuid` VARCHAR(36) NOT NULL,  `balance` DECIMAL(13,2) UNSIGNED NOT NULL,  `last_seen` DATETIME NOT NULL,  PRIMARY KEY (`id`),  UNIQUE INDEX `username_UNIQUE` (`username` ASC),  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC))ENGINE = InnoDB;");
+            PreparedStatement createTransactionsTable = this.con.prepareStatement("CREATE TABLE IF NOT EXISTS `"+this.TBL_TRANSACTIONS+"` (  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,  `account_id` INT UNSIGNED NOT NULL,  `amount` DECIMAL(13,2) NOT NULL,  `timestamp` TIMESTAMP NOT NULL,  PRIMARY KEY (`id`),  INDEX `id_idx` (`account_id` ASC),  CONSTRAINT `account_id`    FOREIGN KEY (`account_id`)    REFERENCES "+this.TBL_ACCOUNTS+" (`id`)    ON DELETE NO ACTION    ON UPDATE NO ACTION)ENGINE = InnoDB;");
+            PreparedStatement createServerBalanceTable = this.con.prepareStatement("CREATE TABLE IF NOT EXISTS `"+this.TBL_SERVER_BALANCE+"` (  `balance` DECIMAL(13,2) NOT NULL,  PRIMARY KEY (`balance`))ENGINE = InnoDB;");
+            PreparedStatement insertServerBalance = this.con.prepareStatement("INSERT INTO "+this.TBL_SERVER_BALANCE+"(balance) VALUES(?);");
+            insertServerBalance.setInt(1,plugin.getConfig().getInt("server_starting_balance",0));
+            
+            try {
+                createAccountsTable.execute();
+                createTransactionsTable.execute();
+                createServerBalanceTable.execute();
+                insertServerBalance.executeUpdate();
+                
+                this.con.commit();
+            } 
+            catch (SQLException ex) {
+                this.con.rollback();
+
+                this.logger.log(Level.SEVERE, null, ex);
+
+                this.con = null;
+
+                return;
+            }
+        }
     }
     
     public boolean hasAccount(String playerName) {
